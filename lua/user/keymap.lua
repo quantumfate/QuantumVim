a5bbbb23ddf6fa7598affbcccf21b2edfe855ee7
Leon Connor Holm <leonconnorholm@gmail.com>
2022-04-16 12:54:59 +0200

    Added commit keymap

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -61,6 +61,9 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+-- Comment
+keymap("n", "<leader>/", "<cmd>lua require('Comment').toggle()<CR>", opts)
+keymap("v", "<leader>/", ":lua require(\"Comment.api\").gc(vim.fn.visualmode())<cr>", opts)
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
