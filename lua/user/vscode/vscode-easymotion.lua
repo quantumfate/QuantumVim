d32bf3e7a674a0e2cd4c88ec0a4621b06aa4fb77
Leon Connor Holm <leonconnorholm@gmail.com>
2023-01-12 13:20:33 +0100

    hop working in vscode

--- lua/user/vscode/vscode-easymotion.lua
+++ lua/user/vscode/vscode-easymotion.lua
@@ -3,18 +3,23 @@ if not status_ok then
   return
 end
 
-
-local vg = vim.g
 -- remap default vim bindings
 local opts = { silent = true , noremap=false }
 local keymap = vim.api.nvim_set_keymap
 local directions = require('hop.hint').HintDirection
-local after_cursor = directions.AFTER_CURSOR, current_line_only = true }
 hop.setup {
   keys = 'etovxqpdygfblzhckisuran'
 }
 local directions = require('hop.hint').HintDirection
-keymap("n", "<leader>w", ":call HopWord<CR>", opts)
-keymap("n", "<leader>W", ":call HopWordMW<CR>", opts)
-keymap("n", "f", ":call HopChar1(" .. after_cursor ..")<CR>", opts)
-keymap("n", "F", ":call HopChar1CurrentLine<CR>", opts)
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
