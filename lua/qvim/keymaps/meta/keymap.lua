312d08e3e6e29f4608df42af24a406881fed5ab0
Leon Connor Holm <leonconnorholm@gmail.com>
2023-03-11 14:20:57 +0100

    another major rewrite

--- lua/qvim/keymaps/meta/keymap.lua
+++ lua/qvim/keymaps/meta/keymap.lua
@@ -3,7 +3,91 @@ local keymap = {}
 local Log = require("qvim.integrations.log")
 local default = require("qvim.keymaps.default")
 local fn_t = require("qvim.utils.fn_t")
-local fn = require("qvim.utils.fn")
+--local fn = require("qvim.utils.fn")
+
+---Ensures that a given table has the default options for keymaps as well as valid parsed options
+---@param _other table
+---@return table metatable the keymap.opts_mt metatable
+local function retrieve_opts_mt(_other)
+    local opts = setmetatable({}, default.keymap_opts)
+    for opt, value in pairs(_other) do
+        if default.valid_keymap_opts[opt] then
+            opts[opt] = value
+        end
+    end
+    return setmetatable(opts, { __index = keymap.opts_mt })
+end
+
+---Ensures that a collection of table has the default optoins as well as parsed options
+---@param _other table
+---@return table metatable the keymap.opts_collection_mt metatable
+local function retrieve_opts_collection_mt(_other)
+    local opts_collection = setmetatable({}, { __index = keymap.opts_collection_mt })
+    for _, binding in pairs(_other) do
+        table.insert(opts_collection, retrieve_opts_mt(binding))
+    end
+    return opts_collection
+end
+---Returns a specific metatable
+---@param _other table|any
+---@return table|nil metatable keymap.opts_mt or keymap.opts_collection_mt or nil
+local function define_other_table(_other)
+    if type(_other) == "table" and type(next(_other)) == "table" then
+        return retrieve_opts_collection_mt(_other)
+    elseif type(_other) == "table" then
+        return retrieve_opts_mt(_other)
+    end
+    Log:warn(
+        "The result of defining a table type is nil because it does not match any of the accepted criteria. Your added keymap will be nil.")
+    return nil
+end
+
+---Checks if a given table _other is present in _this
+---@param _this table metatable for keymap.opts_collection_mt
+---@param _other table metatable for keymap.other_mt
+---@return boolean
+local function opts_mt_is_duplicate(_this, _other)
+    local is_duplicate = false
+    if getmetatable(_this) == keymap.opts_collection_mt and getmetatable(_other) == keymap.opts_mt then
+        for _, pre_value in ipairs(_this) do
+            if pre_value == _other then
+                is_duplicate = true
+            end
+        end
+    end
+    return is_duplicate
+end
+
+---Merges two metatables of keymap.opts_collection_mt
+---@param _this table keymap.opts_collection_mt
+---@param _other table keymap.opts_collection_mt
+---@return table result empty when given tables are not type of the correct metatable  otherwise a unique merge
+local function merge_opts_collection_mt(_this, _other)
+    local result = setmetatable({}, keymap.opts_collection_mt)
+    if getmetatable(_this) == keymap.opts_collection_mt and getmetatable(_other) == keymap.opts_collection_mt then
+        for _, value in ipairs(_other) do
+            if not opts_mt_is_duplicate(_this, value) then
+                fn_t.rawset_debug(result, #result, value, "options collection")
+            end
+        end
+    end
+    return result
+end
+
+---Merges a keymap.opts_collection_mt with a keymap.opts_mt
+---@param _this table keymap.opts_collection_mt
+---@param _other table keymap.opts_mt
+---@return table result empty when given tables are not type of the correct metatable otherwise merged result or the origin _this table
+local function merge_opts_collection_mt_with_opts_mt(_this, _other)
+    local result = setmetatable({}, keymap.opts_collection_mt)
+    local is_opts_collection = getmetatable(_this) == keymap.opts_collection_mt
+    if is_opts_collection and getmetatable(_other) == keymap.opts_mt then
+        if not opts_mt_is_duplicate(_this, _other) then
+            fn_t.rawset_debug(result, #result, _other, "opts collection")
+        end
+    end
+    return is_opts_collection and _this or result
+end
 
 --- The meta table that maps an index function to retrieve
 --- the default keymap options. It implements an __eq meta method
@@ -49,10 +133,14 @@ keymap.opts_mt = {
         end
         return true
     end,
+    __add = function(t1, t2)
+        -- TODO: figure out how this should be merged
+    end
 }
 
 keymap.opts_collection_mt = setmetatable({}, {
-    __index = function(t)
+    __index = function(t, idx)
+        -- TODO: this is just wrong
         local opts_mt_bindings = setmetatable({}, keymap.opts_collection_mt)
         for _, value in ipairs(t) do
             if getmetatable(value) == keymap.opts_mt then
@@ -62,12 +150,24 @@ keymap.opts_collection_mt = setmetatable({}, {
         return opts_mt_bindings
     end,
     __newindex = function(t, idx, other)
-        if getmetatable(other) == keymap.opts_mt then
-            t[idx] = other
+        local other_mt = define_other_table(other)
+        if t[idx] then
+            if other_mt then
+                local pre_mt = getmetatable(t[idx])
+                if not opts_mt_is_duplicate(pre_mt, other_mt) then
+                    t = merge_opts_collection_mt_with_opts_mt(pre_mt, other_mt)
+                    return
+                end
+                t = merge_opts_collection_mt(pre_mt, other_mt)
+            end
+        else
+            fn_t.rawset_debug(t, idx, other_mt, "options collection")
         end
+    end,
+    __add = function(t1, t2)
+        return merge_opts_collection_mt(t1, t2)
     end
 })
-
 keymap.mt = setmetatable({}, {
     __index = function(t, lhs)
         if getmetatable(t) == keymap.opts_mt then
@@ -77,50 +177,23 @@ keymap.mt = setmetatable({}, {
         end
     end,
     __newindex = function(t, lhs, other)
-        ---Set user defined options and fill anything thats not defined with default values
-        ---@param _other table
-        ---@return table metatable the keymap.opts_mt metatable
-        local function retrieve_opts_mt(_other)
-            local opts = setmetatable({}, default.keymap_opts)
-            for opt, value in pairs(_other) do
-                if default.valid_keymap_opts[opt] then
-                    opts[opt] = value
-                end
-            end
-            return setmetatable(opts, { __index = keymap.opts_mt })
-        end
-
-        ---Set multiple user defined bindings
-        ---@param _other table
-        ---@return table metatable the keymap.opts_collection_mt metatable
-        local function retrieve_opts_collection_mt(_other)
-            local opts_collection = setmetatable({}, { __index = keymap.opts_collection_mt })
-            for _, binding in pairs(_other) do
-                table.insert(opts_collection, retrieve_opts_mt(binding))
-            end
-            return opts_collection
-        end
-
-        ---Returns a specific metatable
-        ---@param _other table|any
-        ---@return table|nil metatable keymap.opts_mt or keymap.opts_collection_mt or nil
-        local function define_other_table(_other)
-            if type(_other) == "table" and type(next(_other)) == "table" then
-                return retrieve_opts_collection_mt(_other)
-            elseif type(_other) == "table" then
-                return retrieve_opts_mt(_other)
-            end
-            -- TODO: log eventually
-            return nil
-        end
-
-        if type(other) == "table" then
+        if type(lhs) == "string" and type(other) == "table" then
             local post_t = define_other_table(other)
+            local post_mt = getmetatable(post_t)
             if t[lhs] then
-                -- TODO: merge simple table bindings and complex table bindings and vice verca
-                table.insert(t[lhs], post_t)
+                local current_mt = getmetatable(t[lhs])
+                if (current_mt == keymap.opts_mt and post_mt == keymap.opts_mt) or
+                    (current_mt == keymap.opts_collection_mt and post_mt == keymap.opts_collection_mt) then
+                    t[lhs] = t[lhs] + post_t
+                elseif current_mt == keymap.opts_mt and post_mt == keymap.opts_collection_mt then
+                    t[lhs] = merge_opts_collection_mt_with_opts_mt(post_mt, current_mt)
+                elseif current_mt == keymap.opts_collection_mt and post_mt == keymap.opts_mt then
+                    t[lhs] = merge_opts_collection_mt_with_opts_mt(current_mt, post_mt)
+                else
+                    -- TODO: something went wrong | post_mt == nil
+                end
             else
-                t[lhs] = post_t
+                fn_t.rawset_debug(t, lhs, post_t, "keymap")
             end
         end
     end
@@ -128,93 +201,3 @@ keymap.mt = setmetatable({}, {
 
 
 return keymap
---    __eq = function(t1, t2)
---
---    end,
---    ---This meta method is called when the opts_mt is indexed
---    ---@param _ table not used
---    ---@param opt string the key corresponding to any of the supported default options
---    ---@return boolean|string|integer|nil
---
---    --__call = function(_, opt)
---    --    if default.valid_keymap_opts[opt] then
---    --        return default.keymap_opts[opt]
---    --    else
---    --        Log:error(string.format("Invalid option '%s' for keymap.", opt))
---    --    end
---    --end,
---    ---Ensures that at least the default options are stored in the table
---    ---end sets user defined values if they are supported
---    ---@param t table
---    ---@param lhs string the left hand side of a keymap
---    ---@param other table the keymap corresponding to the left hand side
---    __newindex = function(t, lhs, other)
---        local lhs_is_string = type(lhs) == "string" and not fn.isempty(lhs)
---        local other_is_table = type(other) == "table" and #other > 0
---        if lhs_is_string then
---            if other_is_table then
---                local opts_are_valid = true
---                local invalid_opts = ""
---                local valid_opts = {}
---                for opt, _ in pairs(other) do
---                    if not default.valid_keymap_opts[opt] then
---                        invalid_opts = invalid_opts .. opt .. ", "
---                        opts_are_valid = false
---                    else
---                        table.insert(valid_opts, opt)
---                    end
---                end
---                if opts_are_valid then
---                    local persistent_options = fn_t.rawget_debug(t, lhs, "keymap_opts") or {}
---                    local user_options = {}
---
---                    if #persistent_options > 0 then
---                        if persistent_options.unique then
---                            user_options = persistent_options
---                            Log:warn(string.format(
---                                "Attempted to override a unique keymap '%s'. Its corresponding options '%s' will be kept.",
---                                lhs,
---                                vim.inspect(persistent_options)))
---                        elseif other.unique then
---                            -- discard duplicated mapping
---                            Log:warn(string.format(
---                                "An existing non-unqiue keymap '%s' was overriden by the options: '%s'.", lhs,
---                                vim.inspect(other)))
---
---                            user_options = setmetatable(other, { __index = keymap.rhs_mt })
---                        else
---                            -- keep both mappings
---                            table.insert(user_options, persistent_options)
---                            table.insert(user_options, other)
---                        end
---                    else
---                        user_options = setmetatable(other, { __index = keymap.rhs_mt })
---                        Log:debug(string.format("A new keybinding from '%s' with the options '%' was detected.", lhs,
---                            vim.inspect(user_options)))
---                    end
---                    fn_t.rawset_debug(t, lhs, user_options,
---                        string.format("Setting keymap options: %s", vim.inspect(user_options)))
---                else
---                    Log:error(string.format("Invalid option '%s' for keymap.", invalid_opts:sub(1, -3)))
---                end
---            else
---                if not type(other) == "table" then
---                    Log:error(string.format(
---                        "Declaring keymap options must be done in a table but got '%s'.",
---                        type(other)))
---                elseif not #other > 0 then
---                    Log:error(string.format(
---                        "Empty table detected. No keymap options were set. Table: '%s'.",
---                        other))
---                end
---            end
---        else
---            if not type(lhs) == "string" then
---                Log:error(string.format("The left hand side of a keymap must be a string but is '%s'.", type(lhs)))
---            end
---            if fn.isempty(lhs) then
---                Log:error(string.format("The left hand side of a keymap is an empty string.", type(lhs)))
---            end
---        end
---    end
---
---
+++ lua/qvim/keymaps/meta/old.lua
@@ -0,0 +1,107 @@
+--if #t == 0 then
+--                    t = other_mt
+--                else
+--                    for _, other_value in ipairs(other_mt) do
+--                        local is_duplicate = false
+--                        for _, pre_value in ipairs(pre_mt) do
+--                            if getmetatable(other_value) == keymap.opts_mt and getmetatable(pre_value) == keymap.opts_mt then
+--                                if pre_value == other_value then
+--                                    is_duplicate = true
+--                                end
+--                            end
+--                        end
+--                        if not is_duplicate then
+--                            fn_t.rawset_debug(t, #t + 1, other_value, "Opts collection")
+--                        end
+--                    end
+--                end
+--    __eq = function(t1, t2)
+--
+--    end,
+--    ---This meta method is called when the opts_mt is indexed
+--    ---@param _ table not used
+--    ---@param opt string the key corresponding to any of the supported default options
+--    ---@return boolean|string|integer|nil
+--
+--    --__call = function(_, opt)
+--    --    if default.valid_keymap_opts[opt] then
+--    --        return default.keymap_opts[opt]
+--    --    else
+--    --        Log:error(string.format("Invalid option '%s' for keymap.", opt))
+--    --    end
+--    --end,
+--    ---Ensures that at least the default options are stored in the table
+--    ---end sets user defined values if they are supported
+--    ---@param t table
+--    ---@param lhs string the left hand side of a keymap
+--    ---@param other table the keymap corresponding to the left hand side
+--    __newindex = function(t, lhs, other)
+--        local lhs_is_string = type(lhs) == "string" and not fn.isempty(lhs)
+--        local other_is_table = type(other) == "table" and #other > 0
+--        if lhs_is_string then
+--            if other_is_table then
+--                local opts_are_valid = true
+--                local invalid_opts = ""
+--                local valid_opts = {}
+--                for opt, _ in pairs(other) do
+--                    if not default.valid_keymap_opts[opt] then
+--                        invalid_opts = invalid_opts .. opt .. ", "
+--                        opts_are_valid = false
+--                    else
+--                        table.insert(valid_opts, opt)
+--                    end
+--                end
+--                if opts_are_valid then
+--                    local persistent_options = fn_t.rawget_debug(t, lhs, "keymap_opts") or {}
+--                    local user_options = {}
+--
+--                    if #persistent_options > 0 then
+--                        if persistent_options.unique then
+--                            user_options = persistent_options
+--                            Log:warn(string.format(
+--                                "Attempted to override a unique keymap '%s'. Its corresponding options '%s' will be kept.",
+--                                lhs,
+--                                vim.inspect(persistent_options)))
+--                        elseif other.unique then
+--                            -- discard duplicated mapping
+--                            Log:warn(string.format(
+--                                "An existing non-unqiue keymap '%s' was overriden by the options: '%s'.", lhs,
+--                                vim.inspect(other)))
+--
+--                            user_options = setmetatable(other, { __index = keymap.rhs_mt })
+--                        else
+--                            -- keep both mappings
+--                            table.insert(user_options, persistent_options)
+--                            table.insert(user_options, other)
+--                        end
+--                    else
+--                        user_options = setmetatable(other, { __index = keymap.rhs_mt })
+--                        Log:debug(string.format("A new keybinding from '%s' with the options '%' was detected.", lhs,
+--                            vim.inspect(user_options)))
+--                    end
+--                    fn_t.rawset_debug(t, lhs, user_options,
+--                        string.format("Setting keymap options: %s", vim.inspect(user_options)))
+--                else
+--                    Log:error(string.format("Invalid option '%s' for keymap.", invalid_opts:sub(1, -3)))
+--                end
+--            else
+--                if not type(other) == "table" then
+--                    Log:error(string.format(
+--                        "Declaring keymap options must be done in a table but got '%s'.",
+--                        type(other)))
+--                elseif not #other > 0 then
+--                    Log:error(string.format(
+--                        "Empty table detected. No keymap options were set. Table: '%s'.",
+--                        other))
+--                end
+--            end
+--        else
+--            if not type(lhs) == "string" then
+--                Log:error(string.format("The left hand side of a keymap must be a string but is '%s'.", type(lhs)))
+--            end
+--            if fn.isempty(lhs) then
+--                Log:error(string.format("The left hand side of a keymap is an empty string.", type(lhs)))
+--            end
+--        end
+--    end
+--
