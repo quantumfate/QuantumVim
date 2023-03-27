7c03877bda7d0d04afc66333599cadbf3918b762
Leon Connor Holm <leonconnorholm@gmail.com>
2023-03-27 12:48:20 +0200

    init meta for keymap doen

--- lua/qvim/keymaps/meta/init.lua
+++ lua/qvim/keymaps/meta/init.lua
@@ -4,6 +4,7 @@ local meta = {}
 local binding = require("qvim.keymaps.meta.binding")
 local group = require("qvim.keymaps.meta.group")
 local keymap = require("qvim.keymaps.meta.keymap")
+local mode = require("qvim.keymaps.meta.mode")
 local util = require("qvim.keymaps.meta.util")
     .init(
         binding,
@@ -14,32 +15,11 @@ local util = require("qvim.keymaps.meta.util")
 binding.init(util)
 group.init(util)
 keymap.init(util)
+mode.init(util)
 
-
-local Log = require("qvim.integrations.log")
-
-
----The meta table that holds values grouped by keymap mode adapters
-meta.mode_adapter_mt = setmetatable({}, {
-    ---A key added to this table has to be one of the accepted mode adapters
-    ---@param t table
-    ---@param mode_adapter string
-    ---@param other_keymap_t table
-    __newindex = function(t, mode_adapter, other_keymap_t)
-        if type(other_keymap_t) == "table" then
-            local modes = vim.deepcopy(keymap_mode_adapters)
-            for _mode_adapter, _ in pairs(modes) do
-                modes[_mode_adapter] = true
-            end
-            if modes[mode_adapter] then
-                t[mode_adapter] = util.get_new_keymap_mt(other_keymap_t)
-            else
-                Log:error(string.format("Invalid mode adapter '%s'", other_keymap_t))
-            end
-        else
-            Log:error(string.format("The value corresponding to the mode adapter '%s' must be a table.", mode_adapter))
-        end
-    end
-})
+meta.binding = binding
+meta.group = group
+meta.keymap = keymap
+meta.mode = mode
 
 return meta
--- lua/qvim/keymaps/meta/mode.lua
+++ lua/qvim/keymaps/meta/mode.lua
@@ -1,4 +1,41 @@
 ---@class mode
 local mode = {}
 
+local Log = require("qvim.integrations.log")
+---@class util
+local util = nil
+
+---initializes the mode module with the util factory
+---@param _util util
+---@return mode
+function mode.init(_util)
+    util = _util
+    return mode
+end
+
+---The meta table that holds values grouped by keymap mode adapters
+---TODO: move to file
+mode.mode_adapter_mt = setmetatable({}, {
+    ---A key added to this table has to be one of the accepted mode adapters
+    ---@param t table
+    ---@param mode_adapter string
+    ---@param other_keymap_t table
+    __newindex = function(t, mode_adapter, other_keymap_t)
+        if type(other_keymap_t) == "table" then
+            local modes = vim.deepcopy(keymap_mode_adapters)
+            for _mode_adapter, _ in pairs(modes) do
+                modes[_mode_adapter] = true
+            end
+            if modes[mode_adapter] then
+                t[mode_adapter] = util.get_new_keymap_mt(other_keymap_t)
+            else
+                Log:error(string.format("Invalid mode adapter '%s'", other_keymap_t))
+            end
+        else
+            Log:error(string.format("The value corresponding to the mode adapter '%s' must be a table.", mode_adapter))
+        end
+    end
+})
+
+
 return mode
