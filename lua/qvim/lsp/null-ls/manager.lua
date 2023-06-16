2eb616d4e5bce7463836e30f9bc0b9ae0c239dda
Leon Connor Holm <leonconnorholm@gmail.com>
2023-06-16 11:27:46 +0200

    it's now possible to register packages yourself

--- lua/qvim/lsp/null-ls/manager.lua
+++ lua/qvim/lsp/null-ls/manager.lua
@@ -1,8 +1,10 @@
 local M = {}
 local _ = require('mason-core.functional')
+local registry = require('mason-registry')
 ---@class EventEmitter
 local EventEmitter = require "mason-core.EventEmitter"
 ---@class Package
+---@field new function
 local Package = require "mason-core.package"
 local null_ls_client = require("null-ls").client
 local fn_t = require("qvim.utils.fn_t")
@@ -14,11 +16,12 @@ local fmt = string.format
 -- to reload null ls after installation
 -- probably retry add
 
----Selects the option for each null-ls method by a given `ft_builtins` by the following criteria:
+---Selects the option for each null-ls method for a given `ft_builtins` by the following criteria:
 ---
----- 1. User provided options
----- 2. Common sources between different methods (if there is a source listed in multiple methods of a filetype that source will likely be selected for all methods that list the source)
----- 3. The first available option of a method
+---- User provided options (its entirely the users responsibility to maintain the specified source)
+---- Available mason packages or custom packages
+---- Common sources between different methods (if there is a source listed in multiple methods of a filetype that source will likely be selected for all methods that list the source)
+---- The first available option of a method
 ---
 ---See the source for what is possible: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/lua/null-ls/builtins/_meta/filetype_map.lua
 ---@generic T: table, K:string, V:table
@@ -66,7 +69,7 @@ local function select_null_ls_sources(ft, ft_builtins)
     return selection
 end
 
----comment
+---Based on a given `method` a given `source` will be registered.
 ---@param method string
 ---@param source string
 ---@return boolean
@@ -93,17 +96,28 @@ local function register_sources_on_ft(method, source)
     return true
 end
 
+---Returns an Optional mason package either from the mason registry or creates a new mason package with
+---a provided spec.
+---
+---For more information to custom package hangle, see: https://github.com/williamboman/mason.nvim/blob/main/lua/mason-core/package/init.lua
 ---@param null_ls_source_name string
+---@return Package|nil
 local function resolve_null_ls_package_from_mason(null_ls_source_name)
-    -- taken from mason-null-ls
+    -- taken from mason-null-ls and modified
     -- https://github.com/jay-babu/mason-null-ls.nvim/blob/main/lua/mason-null-ls/automatic_installation.lua
-    local registry = require('mason-registry')
+
     local Optional = require('mason-core.optional')
     local source_mappings = require('mason-null-ls.mappings.source')
 
     return Optional.of_nilable(source_mappings.getPackageFromNullLs(null_ls_source_name)):map(function(package_name)
         if not registry.has_package(package_name) then
             Log:warn(fmt("The null-ls source '%s' is not supported by mason.", null_ls_source_name))
+            local _ok, source_package_spec = pcall(require, "qvim.lsp.null-ls.packages." .. "null_ls_source_name")
+            if _ok then
+                Log:debug(fmt("A custom mason package '%s' was instanciated from the source '%s'.",
+                    source_package_spec.name, null_ls_source_name))
+                return Package.new(source_package_spec)
+            end
             return nil
         end
         local ok, pkg = pcall(registry.get_package, package_name)
@@ -113,18 +127,48 @@ local function resolve_null_ls_package_from_mason(null_ls_source_name)
     end)
 end
 
+---Takes a map of null-ls methods mapped to supported sources and returns a map where individual
+---sources are mapped to one and only one of the following:
+---
+---- a functional mason package that can be used to start the installation
+---- a boolean(false) if the source is not a mason package
+---by a functional Package instance.
+---@generic T: table, K:string, V:table<string>, M:Package|string
+---@param null_ls_builtins T<K,V>
+---@return T<K,T<K,M>>
+local function get_mason_packages_or_null_ls_sources(null_ls_builtins)
+    local table = {}
+
+    for method, sources in pairs(null_ls_builtins) do
+        local collection = {}
+        for _, source in pairs(sources) do
+            resolve_null_ls_package_from_mason(source):if_present(function(package)
+                collection[source] = package
+            end):or_else_get(function()
+                collection[source] = false
+            end)
+        end
+        table[method] = collection
+    end
+
+    return table
+end
+
 
 ---Register all available null-ls builtins for a given filetype and install their corresponding mason package.
 ---@param filetype any
 function M.setup(filetype, lsp_server)
     vim.validate { name = { filetype, "string" } }
     vim.validate { name = { lsp_server, "string" } }
-    local registry = require('mason-registry')
+
     local ft_map = require("qvim.lsp.null-ls._meta").ft_bridge()
     local null_ls_builtins = ft_map[filetype]
-    local selection = select_null_ls_sources(filetype, null_ls_builtins)
 
-    for method, source in pairs(selection) do
+    local method_to_package_info = get_mason_packages_or_null_ls_sources(null_ls_builtins)
+    local selection = select_null_ls_sources(filetype, method_to_package_info)
+
+
+    --[[     for method, source in pairs(selection) do
         if register_sources_on_ft(method, source) then
             resolve_null_ls_package_from_mason(source):if_present(
                 function(package)
@@ -149,9 +193,9 @@ function M.setup(filetype, lsp_server)
                         )
                     end
                 end
-            )
+            ):or_else(filetype)
         end
-    end
+    end ]]
 end
 
 return M
