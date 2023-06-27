44e3498404d3e0b5e6a83a8d563c7c157fde503a
Leon Connor Holm <leonconnorholm@gmail.com>
2023-06-27 09:50:20 +0200

    implemented c and cpp debugger

--- lua/qvim/lang/dap/filetypes/c_cpp.lua
+++ lua/qvim/lang/dap/filetypes/c_cpp.lua
@@ -2,7 +2,43 @@
 local c_cpp = {}
 
 function c_cpp.setup()
-    print("dooo c")
+    -- install codelldb with :MasonInstall codelldb
+    -- configure nvim-dap (codelldb)
+    qvim.integrations.dap.on_config_done = function(dap)
+        dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                -- provide the absolute path for `codelldb` command if not using the one installed using `mason.nvim`
+                command = "codelldb",
+                args = { "--port", "${port}" },
+
+                -- On windows you may have to uncomment this:
+                -- detached = false,
+            },
+        }
+
+        dap.configurations.cpp = {
+            {
+                name = "Launch file",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    local path
+                    vim.ui.input({ prompt = "Path to executable: ", default = vim.loop.cwd() .. "/build/" },
+                        function(input)
+                            path = input
+                        end)
+                    vim.cmd [[redraw]]
+                    return path
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+            },
+        }
+
+        dap.configurations.c = dap.configurations.cpp
+    end
 end
 
 return c_cpp
