6a16d8e30413b3fe7528cc5504b5b2a0420e70d4
Leon Holm <leonconnorholm@gmail.com>
2023-07-06 10:43:25 +0200

    headless

--- lua/qvim/integrations/catppuccin.lua
+++ lua/qvim/integrations/catppuccin.lua
@@ -1,6 +1,9 @@
 ---The catpuccin configuration file
 local M = {}
 
+if in_headless_mode() then
+	return
+end
 local Log = require("qvim.log")
 
 ---Registers the global configuration scope for catpuccin
