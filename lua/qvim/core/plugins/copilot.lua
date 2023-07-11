92c273c64ead5537b6662f39f7016c4a85d2205d
Leon Connor Holm <leonconnorholm@gmail.com>
2023-07-11 12:10:26 +0200

    feat: copilot

---
+++ lua/qvim/core/plugins/copilot.lua
@@ -0,0 +1,63 @@
+---@class copilot : core_meta_plugin
+---@field enabled boolean|fun():boolean|nil
+---@field name string|nil the human readable name
+---@field options table|nil options used in the setup call of a neovim plugin
+---@field keymaps table|nil keymaps parsed to yikes.nvim
+---@field main string the string to use when the neovim plugin is required
+---@field setup fun(self: copilot)|nil overwrite the setup function in core_base
+---@field url string neovim plugin url
+local copilot = {
+  enabled = true,
+  name = nil,
+  options = {
+    panel = {
+      enabled = true,
+      auto_refresh = false,
+      keymap = {
+        jump_prev = "[[",
+        jump_next = "]]",
+        accept = "<CR>",
+        refresh = "gr",
+        open = "<M-CR>",
+      },
+      layout = {
+        position = "bottom", -- | top | left | right
+        ratio = 0.4,
+      },
+    },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = "<M-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
+    filetypes = {
+      yaml = false,
+      markdown = false,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ["."] = false,
+    },
+    copilot_node_command = "node", -- Node.js version must be > 16.x
+    server_opts_overrides = {},
+  },
+  keymaps = {},
+  main = "copilot",
+  setup = nil, -- getmetatable(self).__index.setup(self) to call generic setup with additional logic
+  url = "https://github.com/zbirenbaum/copilot.lua",
+}
+
+copilot.__index = copilot
+
+return copilot
