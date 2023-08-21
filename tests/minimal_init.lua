b6b7f39ca435be8304c859c9f995ac640a91740d
Leon Connor Holm <leonconnorholm@gmail.com>
2023-08-21 10:13:17 +0200

    feat(#7): updatet test init

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -2,6 +2,7 @@ local path_sep = vim.loop.os_uname().version:match("Windows") and "\\" or "/"
 local base_dir = os.getenv("QUANTUMVIM_RTP_DIR")
 local tests_dir = base_dir .. path_sep .. "tests"
 
+vim.opt.rtp:append(base_dir)
 vim.opt.rtp:append(tests_dir)
 require("qvim.bootstrap"):init()
 
